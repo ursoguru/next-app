38c9ed78d63c308078a26cb09bd729451ed69adf
urosguru <garydaccarter@gmail.com>
2020-03-11 08:54:46 +0200

    Second One - testing github integration

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ const Home = () => (
 
     <main>
       <h1 className="title">
-        WelcomFRED23 to <a href="https://nextjs.org">Next.js!</a>
+        WelcomFRED2300 to <a href="https://nextjs.org">Next.js!</a>
       </h1>
 
       <p className="description">
